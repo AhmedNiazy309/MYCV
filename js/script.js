fd9fd3e3a647c2a57680e67aedee881d4ea550d9
Ahmed111222333 <68826015+Ahmed111222333@users.noreply.github.com>
2021-04-14 14:58:42 +0200

    Chart

--- js/script.js
+++ js/script.js
@@ -125,9 +125,11 @@ var myChart = new Chart(libe, {
 var adva = document.getElementById('advanced').getContext('2d');
 var myChart = new Chart(adva, {
     type: 'bar',
+    
     data: {
         labels: ['scss', 'pug', 'axios'],
         datasets: [{
+            label: ['axios'],
             data: [90, 85, 70],
             backgroundColor: bg_color,
             borderColor: border_color,
@@ -135,6 +137,54 @@ var myChart = new Chart(adva, {
         }]
     },
     options: {
+        plugins: {
+            legend: {
+                labels: {
+                    generateLabels: function (chart) {
+                        // Get the default label list
+                        const original = Chart.overrides.pie.plugins.legend.labels.generateLabels;
+                        const labelsOriginal = original.call(this, chart);
+
+                        // Build an array of colors used in the datasets of the chart
+                        var datasetColors = chart.data.datasets.map(function (e) {
+                            return e.backgroundColor;
+                        });
+                        datasetColors = datasetColors.flat();
+
+                        // Modify the color and hide state of each label
+                        labelsOriginal.forEach(label => {
+                            // There are twice as many labels as there are datasets. This converts the label index into the corresponding dataset index
+                            label.datasetIndex = (label.index - label.index % 2) / 2;
+
+                            // The hidden state must match the dataset's hidden state
+                            label.hidden = !chart.isDatasetVisible(label.datasetIndex);
+
+                            // Change the color to match the dataset
+                            label.fillStyle = datasetColors[label.index];
+                        });
+
+                        return labelsOriginal;
+                    }
+                },
+                onClick: function (mouseEvent, legendItem, legend) {
+                    // toggle the visibility of the dataset from what it currently is
+                    legend.chart.getDatasetMeta(
+                        legendItem.datasetIndex
+                    ).hidden = legend.chart.isDatasetVisible(legendItem.datasetIndex);
+                    legend.chart.update();
+                }
+            },
+            tooltip: {
+                callbacks: {
+                    label: function (context) {
+                        const labelIndex = (context.datasetIndex * 2) + context.dataIndex;
+                        return context.chart.data.labels[labelIndex] + ': ' + context.formattedValue;
+                    }
+                }
+            }
+        },
+        responsive: true,
+
         scales: {
             y: {
                 beginAtZero: true
